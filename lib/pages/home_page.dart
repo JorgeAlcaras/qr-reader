5a91103ba5f27557be1873315f42762c6fc8cfe1
Jorge Alcaras <jorge.alcaras@cceo.com.mx>
2024-03-06 15:58:21 -0600

    Feature (QR Reader): :sparkles: A custom navigator bar and a scan button have been added.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_reader/widgets/widgets.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('History'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.delete_forever))
+        ],
+      ),
       body: Center(
         child: Text('Home Page'),
       ),
+
+      bottomNavigationBar: const CustomNavigatorBar(),
+      floatingActionButton: const ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+
     );
   }
 }
